25bd2ffd89c7d52a8925ae72b03247dbc800aad8
Sankalp Chauhan <sankalpchauhan.me@gmail.com>
2020-02-29 15:38:03 +0530

    Update RetryPolicy.java

--- app/src/main/java/me/sankalpchauhan/marsplayassignment/service/repository/RetryPolicy.java
+++ app/src/main/java/me/sankalpchauhan/marsplayassignment/service/repository/RetryPolicy.java
@@ -1,28 +1,77 @@
 package me.sankalpchauhan.marsplayassignment.service.repository;
 
+import android.content.Context;
 import android.util.Log;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
+import me.sankalpchauhan.marsplayassignment.MyApp;
+import me.sankalpchauhan.marsplayassignment.util.Utility;
+import okhttp3.Cache;
+import okhttp3.CacheControl;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 
+import static me.sankalpchauhan.marsplayassignment.MyApp.getContext;
+
 /**
  * Retry Policy and Request Logging
  */
 
 class RetryPolicy {
+
+    private static final Interceptor REWRITE_RESPONSE_INTERCEPTOR = new Interceptor() {
+        @Override
+        public okhttp3.Response intercept(Chain chain) throws IOException {
+            okhttp3.Response originalResponse = chain.proceed(chain.request());
+            String cacheControl = originalResponse.header("Cache-Control");
+            if (cacheControl == null || cacheControl.contains("no-store") || cacheControl.contains("no-cache") ||
+                    cacheControl.contains("must-revalidate") || cacheControl.contains("max-age=0")) {
+                return originalResponse.newBuilder()
+                        .removeHeader("Pragma")
+                        .header("Cache-Control", "public, max-age=" + 5000)
+                        .build();
+            } else {
+                return originalResponse;
+            }
+        }
+    };
+
+    private static final Interceptor REWRITE_RESPONSE_INTERCEPTOR_OFFLINE = new Interceptor() {
+        @Override
+        public okhttp3.Response intercept(Chain chain) throws IOException {
+            Request request = chain.request();
+            if (!Utility.isConnected(getContext())) {
+                request = request.newBuilder()
+                        .removeHeader("Pragma")
+                        .header("Cache-Control", "public, only-if-cached")
+                        .build();
+            }
+            return chain.proceed(request);
+        }
+    };
+
+
     static OkHttpClient okClient() {
         HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
         loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        File httpCacheDirectory = new File(getContext().getCacheDir(), "offlineCache");
+        //10 MB
+        Cache cache = new Cache(httpCacheDirectory, 10 * 1024 * 1024);
         OkHttpClient client = new OkHttpClient().newBuilder()
+                .cache(cache)
                 //Retry Policy
                 .addInterceptor(new ErrorInterceptor())
                 //Logging
                 .addInterceptor(loggingInterceptor)
+                //Caching
+                .addNetworkInterceptor(REWRITE_RESPONSE_INTERCEPTOR)
+                .addInterceptor(REWRITE_RESPONSE_INTERCEPTOR_OFFLINE)
                 .build();
 
         return client;
@@ -66,6 +115,7 @@ class RetryPolicy {
             }
         }
     }
+
 
 
 }
